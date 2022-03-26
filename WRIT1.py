19105f79a1cf254feb0ebc99dc598a141176edea
Rhys Crockett <rhysmorgancrockett@gmail.com>
2022-03-26 11:50:30 +0000

    Set-up a basic credential form that is submitted by each student that registers. Furthermore, a unique ID is generated and displayed after submition. However, this is just using dictionary. It may be more responsible to use a database to hold the credentials and call back and forth to that.

--- WRIT1.py
+++ WRIT1.py
@@ -1,3 +1,4 @@
+import sqlite3 as sql
 
 def app_menu():
     menu = {} # dictionary to hold selection with button
@@ -24,11 +25,12 @@ def app_menu():
 
 def add_record():
     print("add: success")
+    user_application_form()
     # TODO (1) appropriate password-less authentication to gain access to app
     # TODO (2) a form that a user attaches credentials and submits -> user managment table
-    # TODO (3) after submitting for, app generates an encryption key and unique ID
+    # TODO (3) after submitting form, app generates an encryption key and unique ID
     # TODO (4) display uniqiue ID and use either block/stream cipher to encrypt the file (credential(s)) and thus generate ciphertext
-    # TODO (5) generate has values of the ciphertext and unique ID
+    # TODO (5) generate hash values of the ciphertext and unique ID
     # TODO (6) store the ciphertext in a cloud storage bucket
     # TODO (7) save the encryption key and hash value in a premised table -> user management table
 
@@ -44,9 +46,62 @@ def view_record():
     # provides the file/record for viewing/downloading /
     # else file/record is corrupt and cannot be displayed/downloaded
 
-def main():
-    app_menu() # start menu
+def user_application_form():
+    print("Please complete the form and submit to register:")
+    credentials = {}
+    credentials['first_name'] = input("What is your first name? ")
+    credentials['last_name'] = input("What is your last name? ")
+    credentials['email'] = input("What is your email addres? ")
+    creds = credentials.keys()
+    for i in creds:
+        print(f"{i}: {credentials[i]}")
 
+    import random
+    gen_id = [random.randint(0, 9) for i in range(8)]
+    id = ''.join(map(str, gen_id))
+    id = ("st") + id
+
+    while True:
+        submit = input("Check your details are correct and then press 's' to submit. Otherwise press 'e' to exit: ").lower().strip()
+        if submit == 's':
+            import json
+            filename = open(id+".json", "w")
+            json.dump(credentials, filename)
+            filename.close()
+            break
+        elif submit == 'e':
+            break
+        else:
+            print("Invalid Selection!")
+
+    print("Unique ID: ",id, "(Do not lose this!)")
+    
+def user_managment_table():
+    con = sql.connect("database") # create a database
+    c = con.cursor() # control the db
+
+    #c.execute("""
+    #CREATE TABLE IF NOT EXISTS students
+    #([student_id] INTEGER PRIMARY KEY, [first_name] TEXT,
+    #[last_name] TEXT, [email] VARCHAR)
+    #""")
+
+    #c.execute("""
+    #INSERT INTO students (name,
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS keys
+    ([unique_id] INTEGER PRIMARY KEY, [encryption_key] TEXT,
+    [hash_values] TEXT)
+    """)
+
+    con.commit() # save table
+    
+def main():
+    #user_application_form()
+    user_managment_table() # database
+    app_menu() # start menu
+    
     # TODO needed SQL "on premise" user access management table
     
 if __name__ == '__main__':
